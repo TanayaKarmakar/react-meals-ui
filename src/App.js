5c5e53cf3c9c81210b3549ff67f4a73096f595ca
t0k02w6 <tanaya.karmakar@walmartlabs.com>
2023-06-10 22:35:16 +0530

    react-meals

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Header from './Components/Layout/Header';
+import React from 'react';
+import Meals from './Components/Meals/Meals';
+
+function App() {
+  return (
+   <React.Fragment>
+    <Header />
+    <main>
+      <Meals/>
+    </main>
+   </React.Fragment>
+  );
+}
+
+export default App;
